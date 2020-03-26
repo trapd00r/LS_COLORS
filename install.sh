d529dfe9f29359bbcab19b3070b343fbc7325b50
David E. Nedrow <dnedrow@me.com>
2020-03-25 22:14:35 -0400

    Updated install script
    Updated the install script to better handle the ~/.local/share directory.
    
    The script now tries to create the directory if it doesn’t exist. Additional error handling takes care of any exceptions.
    
    Also removed spurious quote mark from lscolor.sh line.
    
    Signed-off-by: David E. Nedrow <dnedrow@me.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,45 @@
 #!/usr/bin/env sh
 #
+# Exit codes
+# 0 - Install was successful.
+# 1 - EPERM - Installation failed due to lack of permissions for ~/.local[/share]
+# 2 - mkdir returned an error other than EPERM.
 
 lscolors_data_dir="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+if [[ ! -d $lscolors_data_dir ]]; then
+	echo "Creating $lscolors_data_dir directory..."
+	mkdir --parents $lscolors_data_dir > /dev/null 2>&1
+	status=$?
+	case $status in
+	0) echo "\nInstallation succeeded.\n"
+		break
+		;;
+	1) cat <<EOF
+
+Install was unable to create the needed directory due to a permissions problem.
+
+Please ensure that you have proper access to:
+
+	$HOME/.local
+
+You can execute the following command and try to install again:
+
+	find ~/.local -type d -exec chmod u+rwx {} +
+
+EOF
+		exit 1
+		;;
+	*) cat << EOF
+
+Install was unable to create the needed directory. The command 'mkdir' return
+the following error code: $status
+
+EOF
+		exit 2
+		;;
+	esac
+fi
 
 if dircolors -b LS_COLORS > lscolors.sh && dircolors -c LS_COLORS > lscolors.csh ; then
   if mv -t "$lscolors_data_dir" lscolors.sh lscolors.csh ; then
@@ -9,7 +47,7 @@ if dircolors -b LS_COLORS > lscolors.sh && dircolors -c LS_COLORS > lscolors.csh
 To enable the colors, add the following line to your shell's start-up script:
 
 For Bourne shell (e.g. ~/.bashrc or ~/.zshrc):
-  . $lscolors_data_dir/lscolors.sh"
+  . $lscolors_data_dir/lscolors.sh
 
 For C shell (e.g. ~/.cshrc):
   . $lscolors_data_dir/lscolors.csh
@@ -17,3 +55,5 @@ For C shell (e.g. ~/.cshrc):
 EOF
   fi
 fi
+
+exit 0
