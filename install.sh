0778dc2df6ac16b5fc44ddd7b8abc8039da75c8a
David E. Nedrow <dnedrow@me.com>
2020-03-30 12:35:45 -0400

    Removed non-Posix double brackets. Converted all tabs to spaces. (For some reason, Sublime defaults to tabs. Ugh.)

--- install.sh
+++ install.sh
@@ -7,38 +7,37 @@
 
 lscolors_data_dir="${XDG_DATA_HOME:-$HOME/.local/share}"
 
-if [[ ! -d $lscolors_data_dir ]]; then
-	echo "Creating $lscolors_data_dir directory..."
-	mkdir --parents $lscolors_data_dir > /dev/null 2>&1
-	status=$?
-	case $status in
-	0) echo "\nInstallation succeeded.\n"
-		break
-		;;
-	1) cat <<EOF
+if [ ! -d $lscolors_data_dir ]; then
+    echo "Creating $lscolors_data_dir directory..."
+    mkdir --parents $lscolors_data_dir > /dev/null 2>&1
+    status=$?
+    case $status in
+    0) echo "\nInstallation succeeded.\n"
+        ;;
+    1) cat <<EOF
 
 Install was unable to create the needed directory due to a permissions problem.
 
 Please ensure that you have proper access to:
 
-	$HOME/.local
+    $HOME/.local
 
 You can execute the following command and try to install again:
 
-	find ~/.local -type d -exec chmod u+rwx {} +
+    find ~/.local -type d -exec chmod u+rwx {} +
 
 EOF
-		exit 1
-		;;
-	*) cat << EOF
+        exit 1
+        ;;
+    *) cat << EOF
 
 Install was unable to create the needed directory. The command 'mkdir' returned
 the following error code: $status
 
 EOF
-		exit 2
-		;;
-	esac
+        exit 2
+        ;;
+    esac
 fi
 
 if dircolors -b LS_COLORS > lscolors.sh && dircolors -c LS_COLORS > lscolors.csh ; then
