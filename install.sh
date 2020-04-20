405eaa7cf8213a573b761ef3cbe9bf070f747a66
David E. Nedrow <dnedrow@me.com>
2020-04-20 16:43:00 -0400

    Simplified install script
    Simplified the error checking and ensured that install exits with the result of last command executed on failures.
    
    Signed-off-by: David E. Nedrow <dnedrow@me.com>

--- install.sh
+++ install.sh
@@ -1,58 +1,39 @@
 #!/usr/bin/env sh
-#
-# Exit codes
-# 0 - Install was successful.
-# 1 - EPERM - Installation failed due to lack of permissions for ~/.local[/share]
-# 2 - mkdir returned an error other than EPERM.
+
+# 3 finger claw technique for POSIX shell programming
+shout() { echo "$0: $*" >&2; }
+die() { shout "${@:2}"; exit $1; }
+try() { "$@" || die $? "cannot $*"; }
+
+mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+status=0
 
 lscolors_data_dir="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 if [ ! -d $lscolors_data_dir ]; then
-    echo "Creating $lscolors_data_dir directory..."
-    mkdir --parents $lscolors_data_dir > /dev/null 2>&1
-    status=$?
-    case $status in
-    0) echo "\nInstallation succeeded.\n"
-        ;;
-    1) cat <<EOF
-
-Install was unable to create the needed directory due to a permissions problem.
-
-Please ensure that you have proper access to:
-
-    $HOME/.local
-
-You can execute the following command and try to install again:
-
-    find ~/.local -type d -exec chmod u+rwx {} +
-
-EOF
-        exit 1
-        ;;
-    *) cat << EOF
-
-Install was unable to create the needed directory. The command 'mkdir' returned
-the following error code: $status
-
-EOF
-        exit 2
-        ;;
-    esac
+    echo "Creating '$lscolors_data_dir' directory..."
+    try mkdir --mode 700 --parents "$lscolors_data_dir"
 fi
 
-if dircolors -b LS_COLORS > lscolors.sh && dircolors -c LS_COLORS > lscolors.csh ; then
-  if mv -t "$lscolors_data_dir" lscolors.sh lscolors.csh ; then
-    cat <<EOF
+echo "Creating the color configuration files."
+if dircolors -b LS_COLORS > "${mytmpdir}/lscolors.sh" && dircolors -c LS_COLORS > "${mytmpdir}/lscolors.csh" ; then
+  echo "Moving color configuration files to '$lscolors_data_dir'."
+  try mv -t "$lscolors_data_dir" "${mytmpdir}/lscolors.sh" "${mytmpdir}/lscolors.csh"
+  cat <<EOF
+
 To enable the colors, add the following line to your shell's start-up script:
 
 For Bourne shell (e.g. ~/.bashrc or ~/.zshrc):
-  . $lscolors_data_dir/lscolors.sh
+  . "$lscolors_data_dir/lscolors.sh"
 
 For C shell (e.g. ~/.cshrc):
-  . $lscolors_data_dir/lscolors.csh
+  . "$lscolors_data_dir/lscolors.csh"
 
 EOF
-  fi
+else
+  status=$?
+  cat <<EOF
+Install was unable to create the color configuration files.
+EOF
+  exit $status
 fi
-
-exit 0
